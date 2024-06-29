b9e310280fa1298230e8ae15b5d22f95e0b85006
Ivandro Neto <ivandroneto1@outlook.com>
2024-06-29 18:30:10 +0100

    Adding pause function

--- FocusTimerV2/actions.js
+++ FocusTimerV2/actions.js
@@ -1,59 +1,53 @@
-
 const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 const STEP = 5
 const timer = document.getElementById('timer')
 let interval
+let isPlaying = false
+
 export function addTime() {
     minutes.innerHTML = Number(minutes.innerHTML) + STEP
 } 
 
 export function subTime() {
     let min = Number(minutes.innerText)
-
-    if(min > 0 ) {
+    if (min > 0) {
         minutes.innerHTML = min - STEP
     }
-
 }
 
 export function play() {
-    let min = Number(minutes.innerText)
-    let sec = Number(seconds.innerText)
-    timer.classList.add('playing')
+    if (!isPlaying) {
+        let min = Number(minutes.innerText)
+        let sec = Number(seconds.innerText)
 
-    if(min && sec == 0 && timer.classList.contains('playing')) {
-        interval = setInterval(() => {
-            if(min <= 0 && sec <= 0) {
-                
-                return
-            }
+        // Inicia apenas se não estiver zero
+        if (min > 0 || sec > 0) {
+            isPlaying = true
+            interval = setInterval(() => {
+                if (min === 0 && sec === 0) {
+                    clearInterval(interval)
+                    isPlaying = false
+                    return
+                }
 
-            if(sec == 0) {
-                min --
-                sec = 60
-            }
-    
-            sec --
-    
-            minutes.innerHTML = min < 10 ? '0' + min : min
-            seconds.innerHTML = sec < 10 ? '0' + sec : sec
-            
-            
-    
-        }, 10)
+                if (sec === 0) {
+                    min--
+                    sec = 60
+                }
+
+                sec--
+
+                minutes.innerHTML = min < 10 ? '0' + min : min
+                seconds.innerHTML = sec < 10 ? '0' + sec : sec
+
+            }, 1000) // intervalo de 1 segundo
+        }
     }
-
 }
 
 export function pause() {
-    let min = Number(minutes.innerText)
-    let sec = Number(seconds.innerText)
+    clearInterval(interval)
     timer.classList.remove('playing')
-
-    minutes.innerHTML = min < 10 ? '0' + min : min
-    seconds.innerHTML = sec < 10 ? '0' + sec : sec
-
-
-    setTimeout(()=>clearInterval(interval), 0);
+    isPlaying = false
 }
